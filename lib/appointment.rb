f21ebd09253ead36cf02313af06fe3383751dd98
Kiana Long <kianajade42.kl@gmail.com>
2021-02-06 23:09:47 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,16 @@
+class Appointment
+  attr_reader :doctor,:patient
+  attr_accessor :date
+  @@all=[]
+  def initialize(date,doctor,patient)
+    @date=date
+    @doctor=doctor
+    @patient=patient
+    @@all<<self
+  end
+
+
+  def self.all
+    @@all
+  end
+end
---
+++ lib/patient.rb
@@ -0,0 +1,26 @@
+class Patient
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(date,doctor)
+          Appointment.new(self)
+        end
+
+  def appointments
+    Appointment.all { |appointment| appointment.doctor == self }
+  end
+
+  def doctors
+    appointments.map(&:doctor)
+  end
+end
